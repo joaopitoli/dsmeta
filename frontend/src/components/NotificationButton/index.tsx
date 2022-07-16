359bde974a34d5995dcf2d11e32b7dbeac55a9cf
Joao Pitoli <joao.contats@gmail.com>
2022-07-16 16:44:37 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
 import vector from '../../assets/img/Vector.svg'
+import { BASE_URL } from '../../utils/request';
 import './style.css'
 
+type Props = {
+    saleId: number;
+}
 
-function NotificationButton() {
+function handleClick(id:number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("SUCESSO")
+    })
+}
+
+function NotificationButton({saleId}:Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={vector} alt="notificar" />
         </div>
     )
