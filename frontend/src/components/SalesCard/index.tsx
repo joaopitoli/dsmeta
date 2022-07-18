091942d95c0b9e3fc1dbb2702e7c6e0f2fcdc176
Joao Pitoli <joao.contats@gmail.com>
2022-07-18 11:54:03 -0300

    Correcao Titulos

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -59,7 +59,7 @@ function SalesCard() {
                             <th className="show992">ID</th>
                             <th className="show576">Data</th>
                             <th>Vendedor</th>
-                            <th className="show992">Visistas</th>
+                            <th className="show992">Visitas</th>
                             <th className="show992">Vendas</th>
                             <th>Total</th>
                             <th>Notificar</th>
